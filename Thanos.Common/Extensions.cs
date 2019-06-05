2eab797fa21c9de10dcf96d3edb89ae89863a8eb
Programazing <chris@thatamazingprogrammer.com>
2019-06-05 09:13:56 -0400

    Add Extensions to Thanos.Common and accompanying tests.

---
+++ Thanos.Common/Extensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Thanos.Common
+{
+    public static class Extensions
+    {
+        public static List<T> ReduceListByHalf<T>(this List<T> input)
+        {
+            var output = new List<T>(input);
+
+            int halfOfCount = output.Count() / 2;
+            var random = new Random();
+
+            var shuffledList = output.OrderBy(i => random.Next()).ToList();
+            shuffledList.RemoveRange(0, halfOfCount);
+
+            output = output.Where(x => shuffledList.Contains(x)).ToList();
+
+            return output;
+        }
+    }
+}
---
+++ Thanos.Tests/Extension.Tests.cs
@@ -0,0 +1,31 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Thanos.Common;
+
+namespace Thanos.Tests
+{
+    [TestFixture]
+    class Extension
+    {
+        
+
+        public Extension()
+        {
+
+        }
+
+        [Test]
+        public static void ReduceListByHalf_ReducesStringList_ByHalf()
+        {
+            var alphabet = new List<string>() { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n",
+            "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
+
+            var halfOfList = alphabet.ReduceListByHalf();
+
+            halfOfList.Should().HaveCount(alphabet.Count / 2);
+        }
+    }
+}
