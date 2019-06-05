017be239c3befc55dbb83ac11f2a481cf623708f
Programazing <chris@thatamazingprogrammer.com>
2019-06-05 12:19:44 -0400

    Add Thanos to Thanos.Common and its related tests.

---
+++ Thanos.Tests/Thanos.Tests.cs
@@ -0,0 +1,14 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Thanos.Common;
+
+namespace Thanos.Tests
+{
+    [TestFixture]
+    class Thanos
+    {
+    }
+}
