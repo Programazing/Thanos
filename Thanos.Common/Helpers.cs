50a0431dfbd2f7bb7ad7ba93f96ed33b82122ad6
Programazing <chris@thatamazingprogrammer.com>
2019-06-06 10:40:10 -0400

    Add Infinity Stones to separate Helper file

---
+++ Thanos.Common/Helpers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Thanos.Common
+{
+    public enum InfinityStones
+    {
+        MindStone,
+        PowerStone,
+        RealityStone,
+        SoulStone,
+        SpaceStone,
+        TimeStone
+    }
+
+    public static class Helpers
+    {
+        
+    }
+}
